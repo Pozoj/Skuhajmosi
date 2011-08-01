1a106dff8c158933b0bbe6edaaa5df8fe06996a5
Ljubomir Markovic <ljm@disru.pt>
2011-08-01 14:14:34 +0200

    teh forgotten fix

--- app/controllers/ingredient_groups_controller.rb
+++ app/controllers/ingredient_groups_controller.rb
@@ -2,7 +2,7 @@ class IngredientGroupsController < InheritedResources::Base
   load_and_authorize_resource
   
   def show
-    @recipes_for_ingredient_groups = nil #Recipe.for_ingredient_group(params[:id])
-    !show
+    @recipes_for_ingredient_groups = Recipe.for_ingredient_group(params[:id])
+    show!
   end
 end
